6150850c6577ad5544535a4e51bf56025c99f332
J0bU <vryhok@gmail.com>
2019-10-28 20:41:02 -0500

    Variable de entorno MongoDB

--- server/config/config.js
+++ server/config/config.js
@@ -18,12 +18,14 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 // ====================
 // BASE DE DATOS
 //====================
+
+
 let urlDataBase;
 
 if(process.env.NODE_ENV === 'dev'){
     urlDataBase = 'mongodb://localhost:27017/cafe';
 }else{
-    urlDataBase = 'mongodb+srv://J0bU:1GOvtCLzv135MNQX@cluster0-5oocy.mongodb.net/Cafe';
+    urlDataBase = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDataBase;
